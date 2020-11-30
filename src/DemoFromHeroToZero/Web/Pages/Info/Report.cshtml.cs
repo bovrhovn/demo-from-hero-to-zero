40d98b11452c7c444c7411e3e703c560d4010a3b
Bojan Vrhovnik <bojan@vrhovnik.net>
2020-11-30 14:06:34 +0100

    adding interfaces and services folder

---
+++ src/DemoFromHeroToZero/Web/Pages/Info/Report.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Web.Pages.Info
+{
+    public class ReportPageModel : PageModel
+    {
+        public void OnGet()
+        {
+            
+        }
+    }
+}
